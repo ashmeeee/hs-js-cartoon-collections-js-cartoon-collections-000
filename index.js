708ff71a8ac83d9f9347314487cf2291d490a968
ashley garcia <arisewithashley@gmail.com>
2019-11-01 20:23:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
-function dwarfRollCall(dwarves) {
+var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+ function dwarfRollCall(callDwarves){
+   var array =[]
+   
+   for (var i=0;i<callDwarves.length;i++) {
+     
+     array.push(`${i+1}. ${callDwarves[i]} `)
+     
+   }
+   
+   return array.join("")
+ }
+ 
+//console.log(dwarfRollCall(callDwarves))
+
+var planeteerCalls = ["earth", "wind", "fire", "water", "heart"]
+
+function summonCaptainPlanet(callPlaneteerCalls){
+	var newArray =[]
+	for(var i=0; i<callPlaneteerCalls.length; i++) {
+	  
+    newArray.push(callPlaneteerCalls[i].toUpperCase() + "!")
+    
+  }
+  return newArray
+}
+// console.log(summonCaptainPlanet(callPlaneteerCalls));
+
+function longPlaneteerCalls(characters){
+  for(var i=0;i<characters.length;i++){
+    if (characters[i].length>4){
+      return true
+    }
+  }
+  return false
 }
 
-function summonCaptainPlanet(planeteerCalls){
+ 
+
+function findTheCheese (cheese) {
+  for(var i=0;i<cheese.length;i++){
+  if (cheese[i]=="cheddar"
+  ||cheese[i]=="gouda"
+  ||cheese[i]=="camembert"
+  ){
+    return cheese[i]
+  }
+}
+return "no cheese!"
 }
 
-function longPlaneteerCalls(words) {
-}
-
-function findTheCheese (foods) {
-}
